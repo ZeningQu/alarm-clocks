a8ffc8560b870eaccbad444aaa4e58e22dc6dc75
Zening Qu <quzening@gmail.com>
2021-10-01 22:15:30 -0700

    alarm clock works! now we just need to load our time!

--- alarm.py
+++ alarm.py
@@ -1,2 +1,12 @@
 from playsound import playsound
-playsound('./sounds/bell.mp3')
+import schedule
+import time
+
+def ring():
+  playsound('./sounds/bell.mp3')
+
+schedule.every().day.at("22:15").do(ring)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
