776bd9deb1d7ab693245342a4ded28743476739a
Zening Qu <quzening@gmail.com>
2021-10-06 05:19:59 -0700

    better comment activities & durations

--- alarm.py
+++ alarm.py
@@ -5,36 +5,36 @@ import time
 def ring():
   playsound('./sounds/bell.mp3')
 
-schedule.every().day.at("03:00").do(ring) # wake up
-schedule.every().day.at("03:30").do(ring) # quiet time
-schedule.every().day.at("04:00").do(ring) # pray
-schedule.every().day.at("04:20").do(ring) # snack
-schedule.every().day.at("04:40").do(ring) # panda daily
-schedule.every().day.at("04:50").do(ring) # L1
-schedule.every().day.at("05:35").do(ring) # break
-schedule.every().day.at("05:45").do(ring) # L2
-schedule.every().day.at("06:30").do(ring) # break
-schedule.every().day.at("06:40").do(ring) # L3
-schedule.every().day.at("07:25").do(ring) # break
-schedule.every().day.at("07:35").do(ring) # L4
-schedule.every().day.at("08:20").do(ring) # cook lunch
-schedule.every().day.at("09:20").do(ring) # nap, Bible
-schedule.every().day.at("10:05").do(ring) # wake up, snack
-schedule.every().day.at("10:25").do(ring) # L5
-schedule.every().day.at("11:10").do(ring) # break
-schedule.every().day.at("11:20").do(ring) # L6
-schedule.every().day.at("12:05").do(ring) # break
-schedule.every().day.at("12:15").do(ring) # L7
-schedule.every().day.at("13:00").do(ring) # iron, vc, dinner
-schedule.every().day.at("13:40").do(ring) # L8
-schedule.every().day.at("14:25").do(ring) # break
-schedule.every().day.at("14:35").do(ring) # L9
-schedule.every().day.at("15:20").do(ring) # break
-schedule.every().day.at("15:30").do(ring) # L10
-schedule.every().day.at("16:15").do(ring) # walk
-schedule.every().day.at("17:15").do(ring) # shower
-schedule.every().day.at("18:00").do(ring) # read
-schedule.every().day.at("19:00").do(ring) # sleep
+schedule.every().day.at("03:00").do(ring) # wake up, tea, breakfast, tea (30 min)
+schedule.every().day.at("03:30").do(ring) # quiet time (30 min)
+schedule.every().day.at("04:00").do(ring) # pray (20 min)
+schedule.every().day.at("04:20").do(ring) # snack, shower, brush, floss, trays (20 min)
+schedule.every().day.at("04:40").do(ring) # panda daily (10 min)
+schedule.every().day.at("04:50").do(ring) # L1 (45 min)
+schedule.every().day.at("05:35").do(ring) # break (10 min)
+schedule.every().day.at("05:45").do(ring) # L2 (45 min)
+schedule.every().day.at("06:30").do(ring) # break (10 min)
+schedule.every().day.at("06:40").do(ring) # L3 (45 min)
+schedule.every().day.at("07:25").do(ring) # break (10 min)
+schedule.every().day.at("07:35").do(ring) # L4 (45 min)
+schedule.every().day.at("08:20").do(ring) # cook, lunch, dishes (1 hr)
+schedule.every().day.at("09:20").do(ring) # nap, Bible (45 min)
+schedule.every().day.at("10:05").do(ring) # wake up, snack, rinse, brush, floss, trays (20 min)
+schedule.every().day.at("10:25").do(ring) # L5 (45 min)
+schedule.every().day.at("11:10").do(ring) # break (10 min)
+schedule.every().day.at("11:20").do(ring) # L6 (45 min)
+schedule.every().day.at("12:05").do(ring) # break (10 min)
+schedule.every().day.at("12:15").do(ring) # L7 (45 min)
+schedule.every().day.at("13:00").do(ring) # iron, vc, dinner, dishes, brush, floss, trays (40 min)
+schedule.every().day.at("13:40").do(ring) # L8 (45 min)
+schedule.every().day.at("14:25").do(ring) # break (10 min)
+schedule.every().day.at("14:35").do(ring) # L9 (45 min)
+schedule.every().day.at("15:20").do(ring) # break (10 min)
+schedule.every().day.at("15:30").do(ring) # L10 (45 min)
+schedule.every().day.at("16:15").do(ring) # walk (1 hr)
+schedule.every().day.at("17:15").do(ring) # shower, cream (45 min)
+schedule.every().day.at("18:00").do(ring) # read (1 hr)
+schedule.every().day.at("19:00").do(ring) # sleep, Bible (8 hr)
 
 while True:
   schedule.run_pending()
